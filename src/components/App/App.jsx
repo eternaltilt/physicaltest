2c6042d686c70810fc4010fd13ff555423c79503
tilt <efejypov@gmail.com>
2022-06-30 16:26:40 +0300

    final fixes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,7 +2,6 @@ import users from '../../users/ForTable.json';
 import UsersTable from '../Table/UsersTable';
 
 function App() {
-console.log('users ===>', users)
   return (
     <div>
       <UsersTable users={users}/>
